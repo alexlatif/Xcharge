c2478e299f3fcc75630c6e8b24cb1279fcd24892
Alessandro Latif <alessandro@latif-khan.com>
2018-03-27 15:42:29 +0200

    route 3

--- xchargeApp/server.js
+++ xchargeApp/server.js
@@ -4,18 +4,36 @@ const express = require('express');
 var path = require('path');
 const app = express();
 
-var url = "http://localhost:3000/";
+// Serve up content from public directory
+app.use(express.static(__dirname + '/public'));
 
 var kovanContract;
 var ropstenContract;
 var rinkebyContract;
 
-// Serve up content from public directory
-app.use(express.static(__dirname + '/public'));
+var timeOut;
+var runningPi = ''
 
-app.get(url + '/kovan', (req, res) => {
-    
+app.get('/ropsten', (req, res) => {
+    runningPi = 'A';
+    //TODO: call kovan to extract deposit
+
+    //TODO: take deposit to ropsten
+    //TODO: call ropsten node
 })
 
+app.get('/rinkeby', (req, res) => {
+    runningPi = 'B';
+    //TODO: call kovan to extract deposit
+
+    //TODO: take deposit to rinkeby 
+    //TODO: call rinkeby node
+})
+
+app.get('/stop', (req, res) => {
+    //TODO: stop running pi
+
+    //TODO: reclaim remaining funds
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
