8da9d18f67b561c1c400cdeed7d2cf107cfad964
Alessandro Latif <alessandro@latif-khan.com>
2018-03-27 14:01:47 +0200

    route start

--- xchargeApp/server.js
+++ xchargeApp/server.js
@@ -4,9 +4,18 @@ const express = require('express');
 var path = require('path');
 const app = express();
 
+var url = "http://localhost:3000/";
 
+var kovanContract;
+var ropstenContract;
+var rinkebyContract;
 
 // Serve up content from public directory
 app.use(express.static(__dirname + '/public'));
+
+app.get(url + '/kovan', (req, res) => {
+    
+})
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
